e8dc5c83965394490093491307c6a3cd0d714dd5
Darley Leal <darleyleal98@gmail.com>
2023-10-13 06:25:11 -0300

    Update in classes: Business Account, SavingAccount and Account.

---
+++ curso-heranca-e-polimorfismo/ExercicioAccount/Account.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace curso_heranca_e_polimorfismo.ExercicioI
+{
+    public class Account
+    {
+        private int Number {  get; set; }
+        private string Holder { get; set; }
+        private double Balance { get; set; }
+
+        public Account() { }
+        public Account(int number, string holder, double balance)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+        }
+        public void Withdraw(double amount)
+        {
+            Balance -= amount;
+        }
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.AppendLine($"Number: {Number}");
+            stringBuilder.AppendLine($"Holder: {Holder}");
+            stringBuilder.AppendLine($"Balance: {Balance}");
+
+            return stringBuilder.ToString();
+        }
+    }
+}
--- curso-heranca-e-polimorfismo/Program.cs
+++ curso-heranca-e-polimorfismo/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using curso_heranca_e_polimorfismo.ExercicioI;
+using System;
+
+namespace Course
+{
+    public class Program
+    {
+        static void Main()
+        {
+            BusinessAccount businessAccount = new BusinessAccount(8010, "Darley Leal", 100.0, 500.0);
+            Console.WriteLine(businessAccount.ToString());
+        }
+    }
+}
